63c20fa31fa09348e5fac882d51ae8a94c0bf87f
Lucas Weslen <matoslopes23>
2021-05-08 00:23:53 -0400

    Criacao dos middlewares

---
+++ README.md
@@ -0,0 +1,3 @@
+# Desafio: Trabalhando com Middlewares
+
+### Neste desafio o propósito era trabalhar a fundo com middlewares no Express foi utilizada a mesma aplicação do desafio anterior
--- src/index.js
+++ src/index.js
@@ -10,19 +10,67 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+
+  const user = users.find( user => user.username===username)
+  if(!user){
+    return response.status(404).json({error:"User not found"})
+  }
+
+  request.user = user;
+  return next();
+
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+  
+  if(user.pro===true){
+    return next();
+    
+  }
+  if(!user.pro && user.todos.length < 10){
+    return  next()
+  }else if(!user.pro && user.todos.length>=10) {
+    return response.status(403).json({error:" Do you not have enough access"})
+  }
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+  const {id} = request.params;
+
+  const user = users.find(user =>user.username===username)
+  if(!user){
+    return response.status(404).json({error:" user not found"});
+  }
+  
+  const checkUuid = validate(id);
+  if(!checkUuid){
+    return response.status(400).json({error:"id is invalid"})
+  }
+
+  const todo = user.todos.find(todo=> todo.id===id)
+  if(!todo){
+      return response.status(404).json({error:"User not found"})
+  }
+  // todo.id = validate;
+  request.user = user;
+  request.todo = todo;
+  return next()
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const {id} = request.params;
+
+  const user = users.find( user => user.id===id);
+  if(!user){
+    return response.status(404).json({error:"user not found"})
+  }
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
